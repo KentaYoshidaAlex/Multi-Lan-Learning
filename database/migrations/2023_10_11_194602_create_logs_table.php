d5c2256d30973cbbe567795808855b50d2183904
multi-language-learning <74769026+KentaYoshidaAlex@users.noreply.github.com>
2024-12-17 00:38:09 +0900

    音声ファイル修正、クイズ挑戦時のログテーブルエラー修正

--- database/migrations/2023_10_11_194602_create_logs_table.php
+++ database/migrations/2023_10_11_194602_create_logs_table.php
@@ -9,22 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    // public function up(): void
-    // {
-    //     Schema::create('logs', function (Blueprint $table) {
-    //         $table->increments('logNo');
-    //         $table->string('loginId_userName', 30);
-    //         $table->string('language', 20);
-    //         $table->integer('quizNo');
-    //         $table->string('question', 100);
-    //         $table->timestamps();
-    //         $table->integer('logRight');
-    //         $table->integer('logWrong');
-    //         $table->integer('clearCount');
-    //         $table->integer('missCount');
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('logNo');
+            $table->string('loginId_userName', 30);
+            $table->string('language', 20);
+            $table->integer('quizNo');
+            $table->string('question', 100);
+            $table->timestamps();
+            $table->integer('logRight');
+            $table->integer('logWrong');
+            $table->integer('clearCount');
+            $table->integer('missCount');
 
-    //     });
-    // }
+        });
+    }
 
     // /**
     //  * Reverse the migrations.
