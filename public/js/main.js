d5433e07d5c741d1534b2dcdb76453f27bfcd3ed
multi-language-learning <74769026+KentaYoshidaAlex@users.noreply.github.com>
2025-01-25 21:05:53 +0900

    アフリカ言語追加、トップページ利用案内ホバー時色変化追加

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+<script>
+const riyouAnnnai = document.getElementById('riyou-annai');
+console.log(riyouAnnnai);
+riyouAnnnai.addEventListener('mouseover',function(){
+  riyouAnnnai.style.color = 'orange'
+})
+
+riyouAnnnai.addEventListener('mouseleave',function(){
+  riyouAnnnai.style.color = 'black'
+})
+</script>
