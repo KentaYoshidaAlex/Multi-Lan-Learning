08f2b1461a557bd3cd9c3ada7646e054ab83c208
multi-language-learning <74769026+KentaYoshidaAlex@users.noreply.github.com>
2026-05-22 06:11:34 +0900

    test: check defaultProviders

--- api/index.php
+++ api/index.php
@@ -4,27 +4,8 @@ error_reporting(E_ALL);
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = require __DIR__.'/../bootstrap/app.php';
+$defaults = Illuminate\Support\ServiceProvider::defaultProviders()->toArray();
 
-// サービスプロバイダーの登録状況を確認
-$providers = $app->getLoadedProviders();
-$hasView = array_key_exists('Illuminate\View\ViewServiceProvider', $providers);
-
-echo "ViewServiceProvider loaded: " . ($hasView ? 'YES' : 'NO') . "<br>";
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-// kernelをbootする
-$kernel->bootstrap();
-
-$providers2 = $app->getLoadedProviders();
-$hasView2 = array_key_exists('Illuminate\View\ViewServiceProvider', $providers2);
-
-echo "ViewServiceProvider after bootstrap: " . ($hasView2 ? 'YES' : 'NO') . "<br>";
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-$kernel->terminate($request, $response);
+echo "<pre>";
+print_r($defaults);
+echo "</pre>";
