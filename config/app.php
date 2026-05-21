f86735e78bcd9488cabba0c257d2482456527af0
multi-language-learning <74769026+KentaYoshidaAlex@users.noreply.github.com>
2026-05-21 21:44:46 +0900

    fix: add ViewServiceProvider explicitly

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Illuminate\View\ViewServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
