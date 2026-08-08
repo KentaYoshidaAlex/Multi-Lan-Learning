c5ff0f454f7d315343bd4f049e1f5e7eed73768c
multi-language-learning <74769026+KentaYoshidaAlex@users.noreply.github.com>
2026-08-08 17:22:19 +0900

    稀に起こる正解だが、不正解表示の問題対応

--- resources/views/quizMain/quizGamen.blade.php
+++ resources/views/quizMain/quizGamen.blade.php
@@ -15,7 +15,7 @@
                         </div>
                         <br>
                         <div class="top-text">
-                            <form method="post" action="{{ route('quizMain') }}" onsubmit="document.getElementById('btn-width-answer').disabled = true;">
+                            <form method="post" action="{{ route('quizMain') }}" onsubmit="setTimeout(function(){ document.getElementById('btn-width-answer').disabled = true; }, 0);">
                                 @csrf
                                 <div class="parent">
                                     <div class="child1-play-gamen">
